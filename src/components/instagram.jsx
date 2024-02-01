2e9b73d1b810ccfe08df180580db3cfdf2a811f0
Ahmad Rafi Maliki <rafimaliki28@gmail.com>
2024-02-01 10:37:24 +0700

    fix img src

--- src/components/instagram.jsx
+++ src/components/instagram.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import instagram from "../assets/instagram.png";
 
 const Instagram = () => {
   return (
@@ -7,7 +8,7 @@ const Instagram = () => {
         <div className="flex items-center hover:bg-gray-200 rounded-lg p-1">
           <div className="w-5 h-5">
             <img
-              src="src/assets/instagram.png"
+              src={instagram}
               alt="https://www.flaticon.com/free-icons/instagram-logo"
             />
           </div>
